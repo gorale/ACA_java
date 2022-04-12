866deca4d5d251da0360c2fc148ce269c78d17f8
Gor <gor.aleksanyan.1197@gmail.com>
2022-04-12 15:29:40 +0400

    Initial commit

---
+++ src/exercise/Ggg.java
@@ -0,0 +1,11 @@
+package exercise;
+
+public class Ggg {
+    public static void main(String[] args) {
+
+    }
+
+    class Human{
+
+    }
+}
---
+++ src/exercise/java.java
@@ -0,0 +1,7 @@
+package exercise;
+
+public class java {
+    public static void main(String[] arg) {
+        System.out.println("Hello world");
+    }
+}
